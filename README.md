c98e6be5c017b88ce28e18b811d8bc5ca8d47541
Jakub Nowicki <kuba@appsilon.com>
2019-02-25 17:22:38 +0100

    setup R instance with different R versions and key path

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
 # r-lambda-workflow
 
 ## Configuring the AWS services
-
-All steps should be done in the previously created instance.
-
 _Important! awc cli should use Python 2!_
 
 1. Upgrade aws cli: `pip install awscli --upgrade --user`
 2. Configure credentials: `aws configure`
-3. Create a key to connect with the aws instances. Substitute its name and location in the `setup_r_instance.sh` file.
-4. Create a security group with port 22 enabled. Substitute its name in the `setup_r_instance.sh` file.
+3. Create a key to connect to the aws instances.
 
 ## Basic R Layer
 
-1. Create EC2 instance, install R and download the installation using `setup_r_instance.sh` script.
-2. Create runtime using `build_runtime.sh` script with R version as a parameter (currently only 3.5.1).
+1. Create EC2 instance, install R and download the installation using `setup_r_instance.sh` script. Provide two parameters: (1) R version (e.g. `3.5.1`); (2) path to the key (e.g. `~/.ssh/key.pem`). Script will download archive with R (`R-{R version}.zip`).
+2. Create runtime using `build_runtime.sh` script with R version as a parameter (e.g. `3.5.1`).
 3. Create a new layer: `aws lambda publish-layer-version --layer-name [layer name] --zip-file fileb://runtime.zip`
 
 ## R packages Layer
--- setup_r_instance.sh
+++ setup_r_instance.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -x
 
+VERSION=$1
+KEY_PATH=$2
+
+KEY_NAME=$(echo $KEY_PATH | rev | cut -d '/' -f 1 | cut -d '.' -f 2 | rev)
+
 # create instance
 
-MyServerID=$(aws ec2 run-instances --image-id ami-4fffc834 --count 1 --instance-type t2.micro --key-name kuba-appsilon --security-groups lambda-instance-group --query 'Instances[0].InstanceId' --output text)
+MyServerID=$(aws ec2 run-instances --image-id ami-4fffc834 --count 1 --instance-type t2.micro --key-name $KEY_NAME --query 'Instances[0].InstanceId' --output text)
 
 MyServerCheckStatus=$(aws ec2 describe-instance-status --instance-id $MyServerID --query 'InstanceStatuses[0].SystemStatus.Status' --output text --output text)
 while [ $MyServerCheckStatus != 'ok' ]; do
@@ -16,10 +21,10 @@ MyServerIP=$(aws ec2 describe-instances --instance-id $MyServerID --query 'Reser
 # install R
 
 ssh-keyscan -T 240 -H $MyServerIP &>> ~/.ssh/known_hosts
-scp -i ~/.ssh/kuba-appsilon.pem build_r.sh ec2-user@$MyServerIP:/home/ec2-user/
-ssh -i ~/.ssh/kuba-appsilon.pem ec2-user@$MyServerIP "chmod +x build_r.sh"
-ssh -i ~/.ssh/kuba-appsilon.pem ec2-user@$MyServerIP "./build_r.sh 3.5.1"
+scp -i $KEY_PATH build_r.sh ec2-user@$MyServerIP:/home/ec2-user/
+ssh -i $KEY_PATH ec2-user@$MyServerIP "chmod +x build_r.sh"
+ssh -i $KEY_PATH ec2-user@$MyServerIP "./build_r.sh $VERSION"
 
 # download R archive
 
-scp -i ~/.ssh/kuba-appsilon.pem ec2-user@$MyServerIP:/opt/R/R-3.5.1.zip .
+scp -i $KEY_PATH ec2-user@$MyServerIP:/opt/R/R-$VERSION.zip .
